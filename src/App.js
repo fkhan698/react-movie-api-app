4491545649ba23a8d4fe1078352d75f9926ff99f
fkhan698 <fkhan698@hotmail.com>
2020-02-05 10:03:41 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Movies from './Movies.js'
 
-function App() {
+
+
+const App = () => {
+  const API_KEY = '664e565dee7eaa6ef924c41133a22b63';
+
+  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("Avengers");
+
+  useEffect(() => {
+    async function getMovies(){
+      const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}`)
+      const data = await response.json()
+      console.log(data.results)
+      setMovies(data.results)
+    }
+    if(query !== "") //Anything typed in will run the function
+      getMovies()
+    
+
+  }, [query])
+
+  const updateMovies = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+    
+  }
+
+  const getSearch = (e) => {
+    e.preventDefault()
+    setQuery(search) //Sets the value of the query to search
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <div className="header"><h1>Movie Database Search</h1>
+     </div>
+    <div className="container">
+      <input className="search-input" onChange={updateMovies} value={search} type="text"></input>
+      <button className="search-button" onClick={getSearch}type="submit"><i className="fa fa-search" aria-hidden="true"></i></button>
     </div>
+      
+        <div className="movies-row">
+        {movies.map(movie => (
+          <Movies
+      
+            key={movie.id}
+            title={movie.title}
+            popularity={movie.popularity}
+            release_date={movie.release_date}
+            image={movie.poster_path}
+          />
+        ))}
+    
+       </div>
+       
+       
+     <footer className="footer">Power by MovieDB API</footer>
+   </div>
   );
 }
 
---
+++ src/Movies.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import MovieInfo from './MovieInfo.js'
+// import 'react-router-dom';
+export default function Movies({title, popularity, release_date, image}) {
+
+
+    return (
+        <div className="movies-items">
+            <h1 className="title">{title}</h1>
+            <img className="image" src={"https://image.tmdb.org/t/p/w500/"+image} alt = ""></img>
+            <strong><p className="popularity">Popularity</p></strong>
+            <p>{popularity}</p>
+            <strong><p className="date">Release Date</p></strong>
+            <p>{release_date.split('/').reverse().join()}</p>
+            <button></button>
+        </div>
+    )
+}
